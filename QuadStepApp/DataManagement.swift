0b014ae2a0974b2745f97dab398505791b0a9bad
Keisuke2162 <applabo.kei@gmail.com>
2020-02-27 03:21:45 +0900

    2020/02/27 CoreDateの以下処理をとりあえず作成 ・データ読み込み ・データ全削除 ・データ追加（1レコード） ・データ更新（1レコード） ・データ削除（1レコード）

--- QuadStepApp/DataManagement.swift
+++ QuadStepApp/DataManagement.swift
@@ -43,5 +43,96 @@ class GeneralDataManagement {
 }
 
 class CoreDataManagement {
+    //セーブデータ格納用
+    var inputData: [HomeData] = []
+    //var saveData = HomeData()
     
+    
+    //CoreDataからデータを取ってくる
+    func DataGet() -> ([HomeData]){
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do {
+            let fetchequest: NSFetchRequest<HomeData> = HomeData.fetchRequest()
+            inputData = try context.fetch(fetchequest)
+            
+        }catch {
+            print("error")
+        }
+        
+        print("\(inputData.count)件のデータを取得しました")
+        return inputData
+    }
+    
+    //CoreDataのデータを全削除
+    func AllDataDelete() {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let fetchRequest: NSFetchRequest<HomeData> = HomeData.fetchRequest()
+        do{
+            let task = try context.fetch(fetchRequest)
+            for i in 0..<task.count {
+                context.delete(task[i])
+            }
+        }catch {
+            print("error")
+        }
+    }
+    
+    //データ追加
+    func DataAdd(saveData: HomeData) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let data = HomeData(context: context)
+        
+        data.uuid = saveData.uuid
+        data.title = saveData.title
+        data.img = saveData.img
+        data.color = saveData.color
+        data.contentText = saveData.contentText
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+    }
+    
+    //指定データ更新
+    func DataUpdate(saveData: HomeData){
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let fetchRequest: NSFetchRequest<HomeData> = HomeData.fetchRequest()
+        
+        do {
+            let result = try context.fetch(fetchRequest)
+            
+            for num in 0 ..< result.count {
+                if result[num].uuid == saveData.uuid {
+                    result[num].title = saveData.title
+                    result[num].img = saveData.img
+                    result[num].color = saveData.color
+                    result[num].contentText = saveData.contentText
+                    
+                    break
+                }
+            }
+            try context.save()
+        }catch {
+            print("error")
+        }
+    }
+ 
+    //指定データ削除
+    func DataDelete(deleteID: String){
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let fetchRequest: NSFetchRequest<HomeData> = HomeData.fetchRequest()
+        
+        do {
+            let result = try context.fetch(fetchRequest)
+            
+            for num in 0 ..< result.count {
+                if result[num].uuid == deleteID {
+                    context.delete(result[num])
+                    
+                    break
+                }
+            }
+            try context.save()
+        }catch {
+            print("error")
+        }
+    }
 }
